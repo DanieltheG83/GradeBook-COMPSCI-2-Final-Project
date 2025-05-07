82195bfcb7fe80dcf2b40fabf39badee360050ff
DanieltheG83 <spikes.gonzalez83@gmail.com>
2025-05-06 20:50:41 -0700

    started class student

--- GradeBook_Project.cpp
+++ GradeBook_Project.cpp
@@ -6,3 +6,23 @@
 #include <iomanip>
 
 using namespace std;
+
+// Student class
+
+class Student {
+
+    private:
+        string name; // student's name
+        int studentID; // studnet's id
+        vector<float> grades; // list of grades
+
+    public:
+    // constructor to initilaze name and ID
+    Student(string name, int studentID) : name(name), studentID(studentID) {}
+
+    // function to add grade
+    void addGrade(float grade) {
+        grades.push_back(grade);
+    }
+
+}
